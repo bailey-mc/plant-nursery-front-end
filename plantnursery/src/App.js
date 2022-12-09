35bc6e87768c932a76efc9031ce29923c164d290
Bailey McDonald <mcdonald.bailey@gmail.com>
2022-12-09 14:14:53 -0600

    new component linked in App function

--- plantnursery/src/App.js
+++ plantnursery/src/App.js
@@ -2,12 +2,22 @@ import {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
 import Plant from './components/plant'
+import New from './components/new'
 
 function App() {
   const [plants, setPlants] = useState([])
+
+    useEffect(()=> {
+      axios
+           .get('herokuapiurl')
+           .then((response)=> {
+            setPlants(response.data)
+           })
+    })
   
   return (
     <div className="App">
+      <New/>
         {
           plants.map((plant) => {
             return (
---
+++ plantnursery/src/components/new.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import axios from 'axios'
+
+const New = (props) => {
+    return(
+        <div>
+            hiiii
+        </div>
+    )
+}
+
+export default New;
