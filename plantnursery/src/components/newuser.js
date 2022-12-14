4370f68e2f66d1d6bc8688ca4531917317083378
Emon Li <emonli.328@gmail.com>
2022-12-14 11:14:40 -0500

    created a login route and moved code into login and newuser component

--- plantnursery/src/components/newuser.js
+++ plantnursery/src/components/newuser.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import '../App.css';
+import { useNavigate } from "react-router-dom";
 
 function App(props) {
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+
+  const navigate = useNavigate();
 
   const triggerCreateUser = (event) => {
     event.preventDefault()
@@ -15,19 +18,26 @@ function App(props) {
     props.handleCreateUser(userObj)
   }
 
+  const handleHaveAccountClick = () => {
+    navigate("/login");
+  };
+
   return (
-    <div className="App" class='formContainer'>
-      <h1 class='formTitle'>Create an Account</h1>
-      <form onSubmit={triggerCreateUser} class='inputForm'>
-        <input type='text' placeholder='username' class='textInput' onChange={(event)=> {setUsername(event.target.value)}}/>
+    <div className='formContainer'>
+      <h1 className='formTitle'>Create an Account</h1>
+      <form onSubmit={triggerCreateUser} className='inputForm'>
+        <input type='text' placeholder='username' className='textInput' onChange={(event)=> {setUsername(event.target.value)}}/>
         <input type='password' placeholder='password' class='textInput' onChange={(event)=> {setPassword(event.target.value)}}/>
         {props.toggleError ?
           <h5 class='errorMsg'>{props.errorMessage}</h5>
           :
           null
         }
-        <input type='submit' value='Register' class='submitBtn'/>
+        <input type='submit' value='Register' className='submitBtn'/>
       </form>
+      <button onClick={handleHaveAccountClick} className="accountBtn">
+        Already have an account?
+      </button>
     </div>
   );
 }
